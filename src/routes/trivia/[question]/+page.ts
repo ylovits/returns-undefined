c2bbf8d86002ca7c241f1233a5b562d93f4985a9
ylovits <ylovits@gmail.com>
2025-11-06 13:40:33 +0100

    question page routing

---
+++ src/routes/trivia/[question]/+page.ts
@@ -0,0 +1,16 @@
+import { questions } from "../questions.sever.js";
+import type { PageLoad } from "./$types";
+
+export const prerender = true;
+
+// Generate entries for all possible question routes
+export async function entries() {
+	return questions.map((_, index) => ({
+		question: index.toString()
+	}));
+}
+
+export const load: PageLoad = async ({ data }) => {
+	// Pass through server data
+	return data;
+};
